70dd7969df8e67acd1f1999ec88da6511e513110
DanielG <danielguerrero562006@gmail.com>
2026-02-14 21:02:07 -0500

    Initial commit of NumberDuel files

---
+++ NumberDuel/Player.hpp
@@ -0,0 +1,25 @@
+#pragma once 
+/**
+ * @file Player.hpp
+ * @author [Daniel Guerrero]
+ * @date [2/14/26]
+ * @brief This file contains the declaration of the Player class, 
+ * representing a players HP, attacks, and abilities.
+ */
+
+
+class Player { 
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
